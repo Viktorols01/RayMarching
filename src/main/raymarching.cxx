f1cfbb12afe56b689772d7450a0cf12a1c1c50d7
Viktor Ols <viktorols01@gmail.com>
2025-01-09 15:00:24 +0100

    attempt to add SDL2

--- src/main/raymarching.cxx
+++ src/main/raymarching.cxx
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <string>
 
+#include <windows.h>
+#include <SDL2/SDL.h>
+
 int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2Test",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     std::cout << "Hej värld!" << std::endl;
     std::string name;
     std::cin >> name;
     std::cout << name << std::endl;
 }
-    
